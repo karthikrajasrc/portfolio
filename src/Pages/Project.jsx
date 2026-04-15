9061724ef8b1b616c0957ef7075bf1b6b0aa5438
karthikrajasrc <karthikrajasrc@gmail.com>
2026-04-15 10:47:15 +0530

    Updated project section

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -5,9 +5,9 @@ import "./Pages.css"
 
 const DEFAULT_ITEMS = [
     {
-      title: 'Client: Yugans Product',
+      title: 'Client: Yugans Product (Freelancing)',
     description: 'Designed and developed a real-time eCommerce platform for the client Yugans Product, with secure JWT-based user authentication, dynamic cart management, and a complete order management system. Monitored website uptime and performance using UptimeRobot, achieving a 90% reduction in load time through performance optimisations.',
-      tech: 'MERN, Tailwind CSS',
+      tech: 'MERN Stack, Tailwind CSS',
      liveDemo: (
     <a href="https://yugansproduct.in/" target="_blank">
       Live Demo
@@ -24,7 +24,7 @@ const DEFAULT_ITEMS = [
     {
       title: 'VibeNet – Social Media Platform',
     description: 'Built a full-stack social media platform with real-time messaging via Socket.io, JWT-based authentication, post creation with likes and comments, follow functionality, and real-time notifications. Integrated UptimeRobot to ping the backend at regular intervals, preventing Render’s free-tier sleep and ensuring consistently fast response times for end users.',
-      tech: 'MERN, Tailwind CSS',
+      tech: 'MERN Stack, Tailwind CSS',
      liveDemo: (
     <a href="https://vibeenet.netlify.app/" target="_blank">
       Live Demo
@@ -39,19 +39,19 @@ const DEFAULT_ITEMS = [
         (<img src={ vibenet} />)
     },
     {
-      title: 'Moview Review',
-      description: 'Built a responsive movie browsing application integrating the TMDB API for real-time movie data including ratings, genres, and descriptions. Implemented advanced search, filter functionality, and dynamic routing using React Router for a seamless experience across all devices also integrated Tailwind CSS for a visually appealing and responsive design.',
-      tech: 'React JS, Tailwind CSS',
+      title: 'Movie Review Web App',
+      description: 'Built a full-stack Movie Review application using the MERN stack and used TMDB API to fetch movie data, featuring secure authentication & authorization, dynamic movie data integration, and user-generated ratings/comments stored in MongoDB. Implemented RESTful APIs with Node.js & Express and deployed with performance monitoring using UptimeRobot.',
+      tech: 'MERN Stack, Tailwind CSS',
      liveDemo: (
-    <a href="https://moviesreviewerr.netlify.app/" target="_blank">
+    <a href="https://moviesreviewe.netlify.app/" target="_blank">
       Live Demo
     </a>
   ),Repolink: (
-    <a href="https://github.com/karthikrajasrc/Movie_review" target="_blank">
+    <a href="https://github.com/karthikrajasrc/Moviereviewer" target="_blank">
       Repo link
     </a>
-  ),
-      id: 2,
+  ), 
+      id: 3,
       image: 
         (<img src={ movie} />)
     }
